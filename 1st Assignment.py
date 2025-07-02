edb643a2850b143c0d5086400edff5b61af8b184
Direct2Ankit <directtoankit@gmail.com>
2025-07-03 03:00:16 +0530

    Add files via upload
    Assignment 1
    Basic Python Concepts
    both Task1 and Task2

---
+++ 1st Assignment.py
@@ -0,0 +1,10 @@
+
+#Task 1: Perform Basic Mathematical Operations
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print(f'Addition: {a+b}\nSubtraction: {a-b}\nMultiplication: {a*b}\nDivision: {a/b}')
+
+#Task 2: Create a Personalized Greeting
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print(f'Hello, {first_name} {last_name}! Welcome to the Python program.')
